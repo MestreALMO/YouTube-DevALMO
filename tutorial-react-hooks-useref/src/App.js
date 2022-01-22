33fc670bedf6390178679a1ed3f0b2140c408491
MestreALMO <lusegardis@gmail.com>
2022-01-22 11:51:59 -0300

    tutorial-react-hooks-useref

--- tutorial-react-hooks-useref/src/App.js
+++ tutorial-react-hooks-useref/src/App.js
@@ -1,23 +1,24 @@
 import { useState, useRef } from "react";
 
 export default function App() {
-  const [state, setState] = useState("");
+  // const [state, setState] = useState("");
   let varLet = "";
   const ref = useRef("");
 
   const data = (e) => {
-    setState(e.target.value);
+    // setState(e.target.value);
+
     varLet = e.target.value;
-    console.log("let: " + varLet);
+    console.log(varLet);
+
     ref.current = e.target.value;
-    console.log("ref: " + ref.current);
   };
 
   return (
     <>
       <h1>DevALMO, YouTube</h1>
       <input type="text" onChange={(e) => data(e)} />
-      <p>My State is {state}</p>
+      {/* <p>My State is {state}</p> */}
       <p>My Let is {varLet}</p>
       <p>My Ref is {ref.current}</p>
     </>
